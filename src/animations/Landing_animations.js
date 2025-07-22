8ee102c6af4607ce989b2aed54b306081daa3a53
Abdul Mannan <abdulmannan5497@gmail.com>
2025-07-22 11:43:25 +0500

    workin on scroll animations

--- src/animations/Landing_animations.js
+++ src/animations/Landing_animations.js
@@ -1,4 +1,5 @@
 import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 
 
@@ -38,3 +39,77 @@ export const animateImageEntrance = (imageRef) => {
     }
   );
 };
+
+
+// export const animate_badges = () => {
+//   const badges = document.querySelectorAll(".badge-highlight");
+
+//   badges.forEach((badge) => {
+//     const bgColor = getComputedStyle(badge).backgroundColor;
+//     badge.style.overflow = "hidden";
+
+//     const highlight = document.createElement("div");
+//     highlight.style.position = "absolute";
+//     highlight.style.top = "-50%";
+//     highlight.style.left = "-50%";
+//     highlight.style.width = "20%";
+//     highlight.style.height = "20%"; // thinner highlight
+//     highlight.style.background = bgColor;
+//     highlight.style.opacity = "0.1"; // more subtle
+//     highlight.style.transform = "rotate(45deg)";
+//     highlight.style.pointerEvents = "none";
+
+//     badge.style.position = "relative";
+//     badge.appendChild(highlight);
+
+//     gsap.to(highlight, {
+//       top: "150%",
+//       left: "150%",
+//       duration: 6, // slower animation
+//       repeat: -1,
+//       ease: "linear"
+//     });
+//   });
+// };
+
+
+gsap.registerPlugin(ScrollTrigger);
+export const animate_scroll_section1 = (targetSelector)=>{
+      gsap.fromTo(
+    targetSelector,
+    { opacity: 0, y: 100 },
+    {
+      opacity: 1,
+      y: 0,
+      duration: 3,
+      ease: "power3.out",
+      scrollTrigger: {
+        trigger: targetSelector,
+        start: "top 80%",
+        end: "top 20%",
+        toggleActions: "play none none reverse",
+        markers: false, // set to true for debug
+      },
+    }
+  );
+}
+
+export const animate_scroll_section2 = (targetSelector)=>{
+      gsap.fromTo(
+    targetSelector,
+    { opacity: 0, x: 100 },
+    {
+      opacity: 1,
+      x: 0,
+      duration: 3,
+      ease: "power3.out",
+      scrollTrigger: {
+        trigger: targetSelector,
+        start: "top 80%",
+        end: "top 20%",
+        toggleActions: "play none none reverse",
+        markers: false, // set to true for debug
+      },
+    }
+  );
+}
