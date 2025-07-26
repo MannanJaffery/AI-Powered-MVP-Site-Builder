e442d0ea5cacb903bc18f93240b3774cda354aaf
Abdul Mannan <abdulmannan5497@gmail.com>
2025-07-26 06:18:05 +0500

    working on the authentication

--- src/firebase.js
+++ src/firebase.js
@@ -16,5 +16,5 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-const provider = new GoogleAuthProvider();
-export { app, auth, db , provider };
+const googleprovider = new GoogleAuthProvider();
+export { app, auth, db , googleprovider };
