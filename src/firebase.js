36ccb8d3d0d5e32e0d7bf4cd544defb2da25ed20
Abdul Mannan <abdulmannan5497@gmail.com>
2025-09-07 01:13:02 +0500

    final push

--- src/firebase.js
+++ src/firebase.js
@@ -8,7 +8,7 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "ai-powered-mvp-builder.firebaseapp.com",
+  authDomain: "bloomqueue.com",
   projectId: "ai-powered-mvp-builder",
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: "327636782437",
